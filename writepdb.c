10b3db20674f076d586a3bafe158ef05d950be7e
Efrem Braun <efrem.braun@gmail.com>
2015-03-30 12:07:47 -0700

    writepdb.c: Fixed error in pdb file formatting

--- writepdb.c
+++ writepdb.c
@@ -26,12 +26,12 @@ void WritePdb(FILE *FilePtrBox0, FILE *FilePtrBox1)
     Countatom++;
     if (Id[I] == 0)
     {
-      fprintf(FilePtrBox0,"ATOM%7d  H              %8.3lf%8.3lf%8.3lf                          H\n",
+      fprintf(FilePtrBox0,"ATOM%7d  H              %8.3lf%8.3lf%8.3lf                         H\n",
         Countatom, X[I], Y[I], Z[I]);
     }
     if (Id[I] == 1)
     {
-      fprintf(FilePtrBox1,"ATOM%7d  H              %8.3lf%8.3lf%8.3lf                          H\n",
+      fprintf(FilePtrBox1,"ATOM%7d  H              %8.3lf%8.3lf%8.3lf                         H\n",
         Countatom, X[I], Y[I], Z[I]);
     }
   }

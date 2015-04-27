25389f4aab6a1831f9327c242b05965ea8103611
Efrem Braun <efrem.braun@gmail.com>
2015-04-27 09:40:53 -0700

    writepdb.c: fixed the printout of the pdb file so that Pymol can read it correctly

--- writepdb.c
+++ writepdb.c
@@ -14,8 +14,8 @@ void WritePdb(FILE *FilePtrBox0, FILE *FilePtrBox1)
   Countmodel++;
 
   // Header
-  fprintf(FilePtrBox0, "MODEL %4d\n", Countmodel);
-  fprintf(FilePtrBox1, "MODEL %4d\n", Countmodel);
+  fprintf(FilePtrBox0, "MODEL     %4d\n", Countmodel);
+  fprintf(FilePtrBox1, "MODEL     %4d\n", Countmodel);
   fprintf(FilePtrBox0, "CRYST1%9.3lf%9.3lf%9.3lf%7.2lf%7.2lf%7.2lf\n", Box[0], Box[0], Box[0], 90.00, 90.00, 90.00);
   fprintf(FilePtrBox1, "CRYST1%9.3lf%9.3lf%9.3lf%7.2lf%7.2lf%7.2lf\n", Box[1], Box[1], Box[1], 90.00, 90.00, 90.00);
 
@@ -26,12 +26,12 @@ void WritePdb(FILE *FilePtrBox0, FILE *FilePtrBox1)
     Countatom++;
     if (Id[I] == 0)
     {
-      fprintf(FilePtrBox0,"ATOM  %5d  H        %4d %8.3lf%8.3lf%8.3lf                          H\n",
+      fprintf(FilePtrBox0,"ATOM  %5d  H        %4d    %8.3lf%8.3lf%8.3lf                       H\n",
         Countatom, Countatom, X[I], Y[I], Z[I]);
     }
     if (Id[I] == 1)
     {
-      fprintf(FilePtrBox1,"ATOM  %5d  H        %4d %8.3lf%8.3lf%8.3lf                          H\n",
+      fprintf(FilePtrBox1,"ATOM  %5d  H        %4d    %8.3lf%8.3lf%8.3lf                       H\n",
         Countatom, Countatom, X[I], Y[I], Z[I]);
     }
   }
